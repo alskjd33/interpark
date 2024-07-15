0094ab4c85500c427cd2a99de0fa8da6ad36389b
alskjd33 <alskjd33@gmail.com>
2024-07-15 13:20:23 +0900

    shopping 스와이퍼

--- js/main.js
+++ js/main.js
@@ -6,5 +6,64 @@ window.onload = function(){
         listShow.classList.toggle("showMypageList");
 
     }
-
+// swiper=====================
+// 프로모션 스와이퍼기능
+const swPromotion = new Swiper(".sw-promotion" , {
+    slidesPerView : 1,
+    spaceBetween: 24,
+    breakpoints:{
+        480 : {slidesPerView : 2
+        } ,
+        760 : {slidesPerView : 2  
+        },
+    },
+    navigation:{
+        nextEL: ".promotion .sw-next",
+        prevEL: ".promotion .sw-prev",
+    },
+    pagination:{
+        el: ".sw-promotion-pg ",
+        clickable: true,
+    },
+    loop : true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+});
+// 쇼핑의 스와이퍼 기능적용
+const swShopping = new Swiper(".sw-shopping", {
+    slidesPerView: 3,
+    grid: {
+      rows: 3,
+      fill: "row",
+    },
+    spaceBetween:10,
+    breakpoints: {
+      900: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
 }
