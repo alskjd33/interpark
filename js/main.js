50208d1a56f9dcea8182865dbf232078ab81da2e
alskjd33 <alskjd33@gmail.com>
2024-07-19 13:20:27 +0900

    북 세팅

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 window.onload = function () {
   // 상단의 마이페이지리스트 기능
-  const iconArrow = document.querySelector(".icon-arrow");
-  const listShow = document.querySelector(".mypage-list");
-  iconArrow.onclick = function () {
-    listShow.classList.toggle("showMypageList");
-  };
-  // swiper=====================
-  // 프로모션 스와이퍼기능
+  const mypageBt = document.querySelector(".mypage-bt");
+  const mypageList = document.querySelector(".mypage-list");
+  mypageBt.addEventListener("click", function () {
+    mypageList.classList.toggle("showMypage");
+  });
+  // swiper ================
+  // 프로모션 스와퍼기능
   const swPromotion = new Swiper(".sw-promotion", {
     slidesPerView: 1,
     spaceBetween: 24,
     breakpoints: {
-      480: { slidesPerView: 2 },
-      760: { slidesPerView: 2 },
+      480: {
+        slidesPerView: 2,
+      },
+      760: {
+        slidesPerView: 2,
+      },
     },
     navigation: {
       nextEl: ".promotion .sw-next",
@@ -28,6 +32,7 @@ window.onload = function () {
       disableOnInteraction: false,
     },
   });
+
   // 쇼핑의 스와이퍼 기능적용
   const swShopping = new Swiper(".sw-shopping", {
     navigation: {
@@ -35,28 +40,28 @@ window.onload = function () {
       prevEl: ".shopping .sw-prev",
     },
     loop: true,
-    slidesPerView: 1.2,
+    slidesPerView: 1.1,
     grid: {
-      rows: 3,
+      rows: 1,
       fill: "row",
     },
     spaceBetween: 10,
     breakpoints: {
       480: {
-        spaceBetween: 26,
+        spaceBetween: 32,
         slidesPerView: 2,
-        slidesPerGroup: 2,
         grid: {
           rows: 1,
         },
+        slidesPerGroup: 3,
       },
-      900: {
+      680: {
         spaceBetween: 32,
         slidesPerView: 3,
-        slidesPerGroup: 3,
         grid: {
           rows: 1,
         },
+        slidesPerGroup: 3,
       },
       1024: {
         spaceBetween: 32,
@@ -78,21 +83,31 @@ window.onload = function () {
   });
   // 투어의 스와이퍼 기능적용
   const swTour = new Swiper(".sw-tour", {
+    // 모바일버전
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
     navigation: {
       nextEl: ".tour .sw-next",
       prevEl: ".tour .sw-prev",
     },
     loop: true,
-    // 모바일버전
-    slidesPerView: 1,
-    grid: {
-      rows: 6,
-      fill: "row",
-    },
     breakpoints: {
-      1024: {
+      480: {
         spaceBetween: 32,
         slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 2,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
         slidesPerGroup: 2,
         grid: {
           rows: 1,
@@ -108,59 +123,57 @@ window.onload = function () {
       },
     },
   });
-  // 티켓의 스와이퍼 기능적용
-  const swTicket = new Swiper(".sw-ticket" ,{
-    slidesPerView: 2,
+  // ticket스와퍼
+  const swTicket = new Swiper(".sw-ticket", {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
     navigation: {
-      nextEl: ".tour .sw-next",
-      prevEl: ".tour .sw-prev",
+      nextEl: ".ticket .sw-next",
+      prevEl: ".ticket .sw-prev",
     },
-    loop:true,
+
+    loop: true,
     breakpoints: {
       480: {
         spaceBetween: 32,
         slidesPerView: 2,
-       
-       
       },
       1024: {
         spaceBetween: 32,
         slidesPerView: 3,
-      
-     
       },
       1280: {
         spaceBetween: 26,
-        slidesPerView:4,
-        
-      
+        slidesPerView: 4,
       },
     },
-  })
-// live스와퍼
-const swLive = new Swiper(".sw-live" , {
-  slidesPerView:4,
-  spaceBetween: 27,
+  });
+  // live스와퍼
+  const swLive = new Swiper(".sw-live", {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    breakpoints: {
+      330: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
 
-  breakpoints: {
-    320: {
-      spaceBetween: 30,
-      slidesPerView: 1,
+      675: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
     },
-    400: {
-      spaceBetween: 30,
-      slidesPerView: 1,
-    },
-    510: {
-      spaceBetween: 25,
-      slidesPerView: 2,
-    },
-     
-    1025:{
-      spaceBetween: 26,
-      slidesPerView:3,
-    }
-  },
-  
-})
+  });
 };
